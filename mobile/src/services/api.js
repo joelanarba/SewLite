14f53de40ab289f9056f3eaa8bf9d519f67ab8e9
joelanarba <anarbajoel@gmail.com>
2025-11-23 07:27:43 +0000

    chore: replace hardcoded API URL with environment variables

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://10.241.97.112:3000'; 
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000'; 
 
 const api = axios.create({
   baseURL: BASE_URL,
